c6d84c191e602baa6067ea4aeac5692ec563aef4
jiaowagn <wangjiaohere@gmail.com>
2018-03-02 15:40:51 +0800

    implement post edit/update/destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find_by_id(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+    redirect_to account_posts_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
